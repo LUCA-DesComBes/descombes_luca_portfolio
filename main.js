c2ae129e7a89d9737c577431312c7a2a1887eba5
LUCA-DesComBes <ldescombes@edenschool.fr>
2024-01-24 13:03:13 +0100

    mobile functionnel almost finished

---
+++ main.js
@@ -0,0 +1,18 @@
+// const namee = document.querySelector("#namme");
+// namee.addEventListener("input", () => {
+//     if(namee <= "a-zA-Z"){
+//         namee.style.borderBottom = "2px solid #00FF00";
+//     }
+//     else if(namee <= ""){
+//         namee.style.borderBottom = "2px solid #FF0000";
+//     }
+//     else {
+//         namee.style.borderBottom = "2px solid #FF0000";
+//     }
+// })
+// const emaile = document.querySelector("#email");
+// emaile.addEventListener("input", () => {
+//     if(emaile <= "a-zA-Z"){
+//         emaile.style.borderBottom = "2px solid #00FF00";
+//     }
+// })
